f200f8cd48214a76bcb4e5c9c04c3d8b1bda6570
Varun Manchanda <33371013+VarunManchanda@users.noreply.github.com>
2020-03-26 16:01:01 +0530

    Update Min Element.cpp

--- Divide&Conquer/Min Element.cpp
+++ Divide&Conquer/Min Element.cpp
@@ -1,1 +1,20 @@
-
+#include <bits/stdc++.h>
+using namespace std;
+int dAndC(int *arr, int s, int e)
+{
+    if(s==e) return arr[s];
+    else
+    {
+        int mid = (s+e)/2;
+        int left = dAndC(arr,s,mid);
+        int right = dAndC(arr,mid+1,e);
+        return min(left,right);
+    }
+}
+int main() {
+    int n; cin >> n;
+    int *arr = new int[n];
+    for(int i=0; i<n; i++) cin >> arr[i];
+    cout << dAndC(arr,0,n-1) << endl;
+    return 0;
+}
